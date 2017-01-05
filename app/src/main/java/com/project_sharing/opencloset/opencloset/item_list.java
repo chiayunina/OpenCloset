83ddaa855c7d370eeb9b6aa442df36a7dc8188b9
Cheng-Hao Tu <s455176@yahoo.com.tw>
2017-01-05 23:10:54 +0800

    list adapter in item_list

--- app/src/main/java/com/project_sharing/opencloset/opencloset/item_list.java
+++ app/src/main/java/com/project_sharing/opencloset/opencloset/item_list.java
@@ -4,43 +4,57 @@ package com.project_sharing.opencloset.opencloset;
  * Created by andytu28 on 1/4/17.
  */
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class item_list extends AppCompatActivity implements View.OnClickListener {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.popup_list_content);
+        setContentView(R.layout.list_page);
+
+        ArrayList<String> aas = new ArrayList<String>();
+        aas.add("123");
+        aas.add("123");
+        ItemListAdapter ila = new ItemListAdapter(this, aas);
+        ListView list_view = (ListView) findViewById(R.id.list_content);
+        list_view.setAdapter(ila);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
+        /*switch (v.getId()) {
             case R.id.discussionContent:
                 startActivity(new Intent(this, item_page.class));
                 break;
+        }*/
+    }
+}
+
+class ItemListAdapter extends ArrayAdapter<String> {
+    public ItemListAdapter(Context context, ArrayList<String> s) {
+        super(context, 0, s);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_list_content, parent, false);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        // getMenuInflater().inflate(R.menu.menu_index, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        //noinspection SimplifiableIfStatement
-        return super.onOptionsItemSelected(item);
+        return convertView;
     }
 }
